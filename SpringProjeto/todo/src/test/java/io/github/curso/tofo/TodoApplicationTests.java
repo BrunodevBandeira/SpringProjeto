f912e92791ea9d5fca1b91f992bff36c7ae532b6
Bruno Bandeira <brunodev29ab@gmail.com>
2021-03-17 19:56:22 -0300

    Iniciando Projeto com SpringBoot

---
+++ SpringProjeto/todo/src/test/java/io/github/curso/tofo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.curso.tofo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
