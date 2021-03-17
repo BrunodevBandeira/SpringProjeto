27465aca80aaeaf6b360dee37ee95fdfbb07aded
Bruno Bandeira <brunodev29ab@gmail.com>
2021-03-17 20:47:57 -0300

    Adicionando Lombok

--- SpringProjeto/todo/src/main/java/io/github/curso/todo/TodoApplication.java
+++ SpringProjeto/todo/src/main/java/io/github/curso/todo/TodoApplication.java
@@ -3,11 +3,14 @@ package io.github.curso.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.curso.todo.model.Todo;
+
 @SpringBootApplication
 public class TodoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+		Todo todo = new Todo();
+		
 	}
-
 }
