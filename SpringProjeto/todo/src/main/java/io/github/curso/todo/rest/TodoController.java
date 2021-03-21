d53121ead3906145331a05800bf2cb7ce3ccd025
Bruno Bandeira <brunodev29ab@gmail.com>
2021-03-20 22:14:03 -0300

    Salvando e consultando com o postman

--- SpringProjeto/todo/src/main/java/io/github/curso/todo/rest/TodoController.java
+++ SpringProjeto/todo/src/main/java/io/github/curso/todo/rest/TodoController.java
@@ -3,6 +3,7 @@ package io.github.curso.todo.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +21,14 @@ public class TodoController {
 	 private TodoRepository repository;
 	 
 	 @PostMapping
-	 public Todo save(@RequestBody Todo todo) {
-		 return repository.save(todo);
-	 }
+	 public Todo save(@RequestBody Todo todo) { //@RequestBody, você usa isso para volar um resultado
+		 return repository.save(todo);          //em Json no postman, por isso que no postman,vc usa
+	 }                                          //o caminho, body ->raw -> Json
 	 
-	 @GetMapping
-	 public Todo getById(Long id) {
-		 return repository.
-				 findById(id) //
+	 @GetMapping("{id}")
+	 public Todo getById(@PathVariable Long id) { //@PathVariable, garante que a URI que o GetMapping
+		 return repository.                       //pegar, sera jogada para dentro do método, com isso 
+				 findById(id) 					  //o postman sabe o caminho que tem que fazer.
 				 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	 }
 }
